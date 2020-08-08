3376fdd4240b47cb33a579ed5548df23d02f6620
Daniil <daniel.eshenko@gmail.com>
2020-08-08 15:24:55 +0300

    add response for header

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,18 @@
-const name = 'Jack'
-
-console.log(`hello ${name}`)
-
-const data = {
-    1:2,
-    3:4,
-    5:6
+const menu = document.querySelector('.menu')
+const burger = document.querySelector('.burger-menu')
+console.log(burger)
+menu.addEventListener('click', () => {
+    console.log(1)
+    toggleMenu()
+})
+burger.addEventListener('click', () => {
+    console.log(1)
+    toggleMenu()
+})
+const toggleMenu = () => {
+    menu.classList.toggle('active')
+    burger.classList.toggle('active')
 }
+console.log(3)
+
+console.log(2)
