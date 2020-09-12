dd41e61ac8e768338747b6ce1ef058783455a1e6
Daniil <daniel.eshenko@gmail.com>
2020-09-12 19:21:00 +0300

    add form validation

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,16 @@
-
-
-
 const menu = document.querySelector('.menu')
 const burger = document.querySelector('.burger-menu')
 const body = document.querySelector('body')
 const playVideo = document.querySelector('.play')
 
+const form = document.getElementById('contact-form')
+const name = document.getElementById('name')
+const email = document.getElementById('email')
+const subject = document.getElementById('subject')
+const budget = document.getElementById('budget')
+const message = document.getElementById('message')
+
+// Mobil menu
 const toggleMenu = () => {
     menu.classList.toggle('active')
     burger.classList.toggle('active')
@@ -19,8 +24,7 @@ burger.addEventListener('click', () => {
     toggleMenu()
 })
 
-
-
+// Slider
 const mySwiper = new Swiper('.swiper-container', {
     direction: 'horizontal',
     loop: true,
@@ -30,16 +34,68 @@ const mySwiper = new Swiper('.swiper-container', {
     },
   })
 
-playVideo.addEventListener('click', () => {
-    console.log('123')
+// Contact form
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    checkInputs()
 })
+const checkInputs = () => {
+    const nameValue = name.value.trim()
+    const emailValue = email.value.trim()
+    const subjectValue = subject.value.trim()
+    const budgetValue = budget.value.trim()
+    const messageValue = message.value.trim()
 
+    if (nameValue === ''){
+        setError(name, 'Поле не заполненно')
+    } else {
+        setSuccess(name)
+    }
 
+    if (emailValue === ''){
+        setError(email, 'Поле не заполненно')
+    } else if (!isEmail(emailValue)){
+        setError(email, 'Не верный email')
+    } else {
+        setSuccess(email)
+    }
 
+    if(subjectValue === ''){
+        setError(subject, 'Поле не заполненно')
+    } else {
+        setSuccess(subject)
+    }
 
-// const getPortfolio = () => {
-//     return fetch('http://localhost:3000/portfolio')
-//     .then(response => response.json())
-//     .then(portfolio => portfolio)
-// }
+    if(budgetValue === ''){
+        setError(budget, 'Поле не заполненно')
+    } else {
+        setSuccess(budget)
+    }
+
+    if(messageValue === ''){
+        setError(message, 'Поле не заполненно')
+    } else {
+        setSuccess(message)
+    }
+}
+
+const setError = (input, message) => {
+    const formControl = input.parentElement;
+    const error = formControl.querySelector('.form__error')
+    input.className = 'form__input_error'
+    error.innerText = message
+}
+
+const setSuccess = (input) => {
+    const formControl = input.parentElement;
+    const error = formControl.querySelector('.form__error')
+    input.className = 'form__input_success'
+    error.innerText = ''
+}
+
+const isEmail = (email) => {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
 
